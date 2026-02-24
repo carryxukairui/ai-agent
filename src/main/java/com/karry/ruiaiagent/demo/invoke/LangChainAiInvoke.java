8e5084f75f11269d52375eda361815d80e19efda
carry <2861188131@qq.com>
2026-02-24 10:49:43 +0800

    使用pgVector向量存储数据

--- src/main/java/com/karry/ruiaiagent/demo/invoke/LangChainAiInvoke.java
+++ src/main/java/com/karry/ruiaiagent/demo/invoke/LangChainAiInvoke.java
@@ -1,17 +1,17 @@
 package com.karry.ruiaiagent.demo.invoke;
 
-import com.karry.ruiaiagent.service.TestApiKey;
-import dev.langchain4j.community.model.dashscope.QwenChatModel;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-
-public class LangChainAiInvoke {
-
-    public static void main(String[] args) {
-        ChatLanguageModel qwenModel = QwenChatModel.builder()
-                .apiKey(TestApiKey.API_KEY)
-                .modelName("qwen-max")
-                .build();
-        String answer = qwenModel.chat("我是程序员鱼皮，这是编程导航 codefather.cn 的原创项目教程");
-        System.out.println(answer);
-    }
-}
+//import com.karry.ruiaiagent.service.TestApiKey;
+//import dev.langchain4j.community.model.dashscope.QwenChatModel;
+//import dev.langchain4j.model.chat.ChatLanguageModel;
+//
+//public class LangChainAiInvoke {
+//
+//    public static void main(String[] args) {
+//        ChatLanguageModel qwenModel = QwenChatModel.builder()
+//                .apiKey(TestApiKey.API_KEY)
+//                .modelName("qwen-max")
+//                .build();
+//        String answer = qwenModel.chat("我是程序员鱼皮，这是编程导航 codefather.cn 的原创项目教程");
+//        System.out.println(answer);
+//    }
+//}
