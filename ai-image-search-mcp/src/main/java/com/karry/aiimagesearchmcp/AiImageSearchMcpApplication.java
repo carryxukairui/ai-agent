8b031a8de0bd317412c269cdf0a007c9f3fcc897
carry <2861188131@qq.com>
2026-02-28 17:02:53 +0800

    mcp-模块创建-图片搜索服务

---
+++ ai-image-search-mcp/src/main/java/com/karry/aiimagesearchmcp/AiImageSearchMcpApplication.java
@@ -0,0 +1,23 @@
+package com.karry.aiimagesearchmcp;
+
+import com.karry.aiimagesearchmcp.tools.ImageSearchTool;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AiImageSearchMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiImageSearchMcpApplication.class, args);
+    }
+    @Bean
+    public ToolCallbackProvider imageSearchTools(ImageSearchTool imageSearchTool) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(imageSearchTool)
+                .build();
+    }
+}
+
---
+++ ai-image-search-mcp/src/test/java/com/karry/aiimagesearchmcp/AiImageSearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karry.aiimagesearchmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiImageSearchMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
