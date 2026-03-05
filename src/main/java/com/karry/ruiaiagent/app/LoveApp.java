acc73d6d32607dfa67a645668b50f76b2fcead45
carry <2861188131@qq.com>
2026-03-05 09:24:57 +0800

    AI 应用接口开发

--- src/main/java/com/karry/ruiaiagent/app/LoveApp.java
+++ src/main/java/com/karry/ruiaiagent/app/LoveApp.java
@@ -26,6 +26,7 @@ import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Set;
@@ -406,6 +407,17 @@ public class LoveApp {
         String content = response.getResult().getOutput().getText();
         log.info("content: {}", content);
         return content;
+    }
+
+
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return client
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .stream()
+                .content();
     }
 
 }
